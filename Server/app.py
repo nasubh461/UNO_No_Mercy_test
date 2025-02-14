b50ef05b935b9dd23fbe4d70c32826957f8c8cc3
deepjyoti7147 <deepjyoti.college@gmail.com>
2025-02-14 12:22:18 +0530

    added the inital logic of the game

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+import random
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask in Docker!"
+# Card colors and values
+COLORS = ['Red', 'Green', 'Blue', 'Yellow']
+VALUES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two']
+SPECIAL_CARDS = ['Wild', 'Wild Draw Four']
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+
+# Generate deck
+def generate_deck():
+    deck = []
+    for color in COLORS:
+        for value in VALUES:
+            deck.append((color, value))
+            if value != '0':
+                deck.append((color, value))
+    for special in SPECIAL_CARDS:
+        deck.extend([(special, None)] * 4)
+    random.shuffle(deck)
+    return deck
+
+
+# Need to find a way to save game s
