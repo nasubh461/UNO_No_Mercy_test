5f2988d99d3697dcf6269c3005d72083fdb3af67
Deepjyoti Purkayastha <deepjyoti.college@gmail.com>
2025-02-26 22:28:25 +0530

    N.A

--- app.py
+++ app.py
@@ -427,7 +427,4 @@ def debug():
     })
 
 if __name__ == '__main__':
-    socketio.run(app, host='10.0.0.210', port=5000, debug=True, allow_unsafe_werkzeug=True)
-
-
-#CHECK GITHUB WORKFLOW
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
